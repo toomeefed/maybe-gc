e457002cbd852a0d1e0e6762884bb0fe9917e33b
楼教主 <cik520@qq.com>
2018-05-15 00:32:05 +0800

    add comments

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const ms = require('ms');
 const debug = require('debug')('maybe:gc');
 
 const defaultRate = 10 * 60 * 1000; // 10 minutes
-const queue = [];
-let intervalTimes = 0;
-let realRate = 0;
+const queue = []; // gc objects queue
+let intervalTimes = 0; // processing times
+let realRate = 0; // the frequency of the first call
 
 /**
- * gc
- *
+ * gc function
  * @param {object} instance
  * @param {function} instance.gc
- * @param {string} instance.gcType
- * @param {number|string} interval
+ * @param {string} [instance.gcType]
+ * @param {number|string} [interval]
+ * @param {number|string} [rate]
  */
 function gc(instance = {}, interval = defaultRate, rate = defaultRate) {
   assert(is.function(instance.gc), 'instance.gc must be a function');
@@ -29,7 +29,7 @@ function gc(instance = {}, interval = defaultRate, rate = defaultRate) {
     if (intervalTimes % num !== 0) {
       return;
     }
-    debug(`run gc, type: ${instance.gcType || 'unknown'}`);
+    debug('run gc, type: %s', instance.gcType || 'unknown');
     instance.gc();
   });
 
@@ -37,7 +37,7 @@ function gc(instance = {}, interval = defaultRate, rate = defaultRate) {
     realRate = is.string(rate) ? ms(rate) : rate;
     const timer = setInterval(() => {
       intervalTimes += 1;
-      queue.forEach(gcQueueHandle => gcQueueHandle());
+      queue.forEach(handle => handle());
     }, realRate);
     timer.unref();
   }
